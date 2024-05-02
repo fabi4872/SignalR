59a6317b21cdcae8e91253abef708ba39e1e85b0
Fabian Salomone <fabi4872@gmail.com>
2024-05-02 01:39:03 -0300

    Nuevo endpoint con Api para consumir el servicio de forma externa

--- SignalR/Controllers/VentaVehiculoHubApiController.cs
+++ SignalR/Controllers/VentaVehiculoHubApiController.cs
@@ -1,47 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 using SignalR.Models;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SignalR.Controllers
 {
-    public class HomeController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VentaVehiculoHubApiController : ControllerBase
     {
         private readonly IHubContext<VentaVehiculoHub> _ventaVehiculoHub;
         private readonly IConfiguration _configuration;
 
-        public HomeController(IHubContext<VentaVehiculoHub> ventaVehiculoHub, IConfiguration configuration)
+        public VentaVehiculoHubApiController(IHubContext<VentaVehiculoHub> ventaVehiculoHub, IConfiguration configuration)
         {
             _ventaVehiculoHub = ventaVehiculoHub;
             _configuration = configuration;
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        public IActionResult VentaVehiculoForm()
-        {
-            return View();
-        }
-
+        [HttpPost]
         public async Task<IActionResult> AddVentaVehiculo(VentaVehiculo ventaVehiculo)
         {
-            if (ModelState.IsValid && ventaVehiculo.Tipo != null && ventaVehiculo.Marca != null && ventaVehiculo.Modelo != null && ventaVehiculo.Precio != null)
+            if (ventaVehiculo.Tipo != null && ventaVehiculo.Marca != null && ventaVehiculo.Modelo != null && ventaVehiculo.Precio != null)
             {
-                ventaVehiculo.Origen = "INTERNO";
+                ventaVehiculo.Origen = "EXTERNO";
                 ventaVehiculo.Tipo = ventaVehiculo.Tipo.ToUpper();
                 ventaVehiculo.Marca = ventaVehiculo.Marca.ToUpper();
                 ventaVehiculo.Modelo = ventaVehiculo.Modelo.ToUpper();
@@ -59,14 +46,8 @@ namespace SignalR.Controllers
                         ventaVehiculo.Marca, ventaVehiculo.Modelo, precioFormateado, ventaVehiculo.Fecha);
                 }
             }
-            
-            return View("VentaVehiculoForm");
-        }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return Ok();
         }
     }
 }
--- SignalR/Models/VentaVehiculo.cs
+++ SignalR/Models/VentaVehiculo.cs
@@ -1,13 +1,18 @@
-﻿namespace SignalR.Models
+﻿using System.Text.Json.Serialization;
+
+namespace SignalR.Models
 {
     public class VentaVehiculo
     {
+        [JsonIgnore]
         public string Id { get; set; }
+        [JsonIgnore]
         public string Origen { get; set; }
         public string Tipo { get; set; }
         public string Marca { get; set; }
         public string Modelo { get; set; }
         public string Precio { get; set; }
+        [JsonIgnore]
         public string Fecha { get; set; }
     }
 }
